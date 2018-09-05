ee97553082cf6e281e049b53dba521ee5b844549
premotectrl <premotectrl@gmail.com>
2018-09-05 12:18:19 +0200

    Add files via upload

---
+++ Car_sim/Data/Config/Settings.sce
@@ -0,0 +1,7 @@
+#INFOFILE1.1 - Do not remove this line!
+FileIdent = Scenario-Config 1.0
+FileCreator = CarMaker for Simulink 6.0 2017-4-18
+
+Scene.Country = DEU
+Scene.Country2 =
+Scene.DrivingSide = right
---
+++ Car_sim/Data/Script/Examples/RemoteControlIPGMovie.tcl
@@ -0,0 +1,34 @@
+### Helper functions
+proc IPGMovie_Count {} {
+    set Number [llength [WInfoInterps "*MOVIE"]]
+    return $Number
+}
+
+
+### Main functions to be called from the simulation
+SigHandler SetMovie_CreateWindow {args} {
+    if {[IPGMovie_Count] <= 0} {
+        Movie start
+    }
+    Movie window create {*}$args
+}
+
+
+# e.g. for TestRun ActiveLights
+SigHandler SetMovie_Background {backgr} {
+    if {[IPGMovie_Count] <= 0} {
+        Movie start
+    }
+    Movie background select $backgr
+}
+
+
+
+SigHandler SetMovie_Camera {args} {
+    if {[IPGMovie_Count] <= 0} {
+        Movie start
+    }
+    Movie camera select {*}$args
+}
+
+
